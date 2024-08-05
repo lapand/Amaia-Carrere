f0962aab2ecdabcdfa53248a4038f8f38a0ee709
Laporte Andoni <lapand@hotmail.fr>
2024-08-05 20:21:58 +0200

    fixed building errors

--- src/app/components/Fade.tsx
+++ src/app/components/Fade.tsx
@@ -29,87 +29,85 @@ const HIDDEN = 2;
 const ENTERING = 3;
 const LEAVING = 4;
 
-const Fade = forwardRef<HTMLDivElement, FadeProps>(
-  (
-    {
-      children,
-      visible,
-      duration = 500,
-      delay = 0,
-      animateEnter = false,
-      from = { opacity: 0, x: 0, y: 0, z: 0 },
-      style,
-      className,
-    },
-    ref
-  ) => {
-    const childRef = useRef(children);
-    // state représente l'état du composant Fade
-    const [state, setState] = useState(
-      visible ? (animateEnter ? ENTERING : VISIBLE) : HIDDEN
-    );
+const Fade = forwardRef<HTMLDivElement, FadeProps>(function FadeComponent(
+  {
+    children,
+    visible,
+    duration = 500,
+    delay = 0,
+    animateEnter = false,
+    from = { opacity: 0, x: 0, y: 0, z: 0 },
+    style,
+    className,
+  },
+  ref
+) {
+  const childRef = useRef(children);
+  // state représente l'état du composant Fade
+  const [state, setState] = useState(
+    visible ? (animateEnter ? ENTERING : VISIBLE) : HIDDEN
+  );
 
-    if (visible) {
-      childRef.current = children;
-    }
-
-    useEffect(() => {
-      if (!visible) {
-        setState((s) => (s === VISIBLE ? LEAVING : HIDDEN));
-      } else {
-        setState((s) => (s === HIDDEN ? ENTERING : VISIBLE));
-      }
-    }, [visible]);
-
-    useEffect(() => {
-      if (state === LEAVING) {
-        const timer = setTimeout(() => {
-          setState(HIDDEN);
-        }, duration);
-        return () => {
-          clearTimeout(timer);
-        };
-      } else if (state === ENTERING) {
-        document.body.offsetHeight;
-        setState(VISIBLE);
-      }
-    }, [state]);
-
-    if (state === HIDDEN) {
-      return null;
-    }
-
-    // VISIBLE => style opacity 1 et présence dans le DOM
-    // ENTERING & LEAVING => style opacity 0 et présence dans le DOM
-    // HIDDEN => style opacity 0 et suppression du DOM
-
-    let transitionStyle: CSSProperties = {
-      transitionProperty: 'opacity transform',
-      transitionDuration: `${duration}ms`,
-      transitionTimingFunction: 'ease-in-out',
-      transitionDelay: `${delay}ms`,
-    };
-    if (state !== VISIBLE) {
-      if (from.opacity !== undefined) {
-        transitionStyle.opacity = from.opacity;
-      }
-      transitionStyle.transform = `translate3d(${from.x ?? 0}px, ${
-        from.y ?? 0
-      }px, ${from.z ?? 0}px)`;
-    }
-
-    // L'ordre de déstructuration des styles a de l'importance :
-    // On déstructurera transitionStyle en second afin qu'il vienne overwrite les styles apportés et permettre un effet de transition correct
-    return (
-      <div
-        ref={ref}
-        style={{ ...style, ...transitionStyle }}
-        className={className}
-      >
-        {childRef.current}
-      </div>
-    );
+  if (visible) {
+    childRef.current = children;
   }
-);
+
+  useEffect(() => {
+    if (!visible) {
+      setState((s) => (s === VISIBLE ? LEAVING : HIDDEN));
+    } else {
+      setState((s) => (s === HIDDEN ? ENTERING : VISIBLE));
+    }
+  }, [visible]);
+
+  useEffect(() => {
+    if (state === LEAVING) {
+      const timer = setTimeout(() => {
+        setState(HIDDEN);
+      }, duration);
+      return () => {
+        clearTimeout(timer);
+      };
+    } else if (state === ENTERING) {
+      document.body.offsetHeight;
+      setState(VISIBLE);
+    }
+  }, [state]);
+
+  if (state === HIDDEN) {
+    return null;
+  }
+
+  // VISIBLE => style opacity 1 et présence dans le DOM
+  // ENTERING & LEAVING => style opacity 0 et présence dans le DOM
+  // HIDDEN => style opacity 0 et suppression du DOM
+
+  let transitionStyle: CSSProperties = {
+    transitionProperty: 'opacity transform',
+    transitionDuration: `${duration}ms`,
+    transitionTimingFunction: 'ease-in-out',
+    transitionDelay: `${delay}ms`,
+  };
+  if (state !== VISIBLE) {
+    if (from.opacity !== undefined) {
+      transitionStyle.opacity = from.opacity;
+    }
+    transitionStyle.transform = `translate3d(${from.x ?? 0}px, ${
+      from.y ?? 0
+    }px, ${from.z ?? 0}px)`;
+  }
+
+  // L'ordre de déstructuration des styles a de l'importance :
+  // On déstructurera transitionStyle en second afin qu'il vienne overwrite les styles apportés et permettre un effet de transition correct
+  return (
+    <div
+      ref={ref}
+      style={{ ...style, ...transitionStyle }}
+      className={className}
+    >
+      {childRef.current}
+    </div>
+  );
+});
 
 export default Fade;
