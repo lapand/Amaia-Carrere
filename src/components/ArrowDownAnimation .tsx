1ea86c709255c48ecea110f415aecb95db98fab9
Laporte Andoni <lapand@hotmail.fr>
2025-04-07 15:35:22 +0200

    debug iOS Safari

--- src/components/ArrowDownAnimation .tsx
+++ src/components/ArrowDownAnimation .tsx
@@ -2,6 +2,11 @@ const ArrowDownAnimation = () => {
   return (
     <span
       className="relative w-10 h-20"
+      style={{
+        willChange: 'transform, opacity',
+        contain: 'paint',
+        transform: 'translateZ(0)',
+      }}
     >
       <span className="scroll-down-btn scroll-down-btn1">
         <span></span>
