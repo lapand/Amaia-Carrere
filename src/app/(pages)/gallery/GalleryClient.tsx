a0feeb6041b13bfaa0de53327ad75bf54341fe52
Laporte Andoni <lapand@hotmail.fr>
2025-04-08 14:29:06 +0200

    debug mobile UI in prod

--- src/app/(pages)/gallery/GalleryClient.tsx
+++ src/app/(pages)/gallery/GalleryClient.tsx
@@ -30,13 +30,13 @@ console.log(images);
 
   return (
     <div className="flex-1 flex flex-col items-center gap-10 sm:gap-16">
-      <div className="relative">
-        <h1 className="text-primary-600 regards text-4xl sm:text-4.5xl 2xl:text-5xl">
-          <span className="inline-block underline-custom after:h-[0.28rem] after:bottom-2 2xl:after:bottom-3">
+      {/* <div className="relative"> */}
+        <h1 className="text-primary-600 regards text-4xl sm:text-4.5xl 2xl:text-5xl underline-custom after:h-[0.28rem] after:bottom-0">
+          {/* <span className=""> */}
             Galerie
-          </span>
+          {/* </span> */}
         </h1>
-      </div>
+      {/* </div> */}
       <div className="lg:w-[55rem] xl:w-[75rem] 2xl:w-[80rem] 3xl:w-[90rem] columns-2 md:columns-3 xl:columns-4 gap-3 lg:gap-5">
         {galleryItems}
       </div>
